937723307d98e338197cf61770b2bf2e60682cdd
Leonardo Trindade <ltrindade@inf.ufsm.br>
2023-03-03 19:50:41 -0300

    feature: added swagger package to the project

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,19 +1,30 @@
 import { Controller, UseGuards, Request, Post } from '@nestjs/common';
+import { ApiOkResponse, ApiTags } from '@nestjs/swagger';
 import { AuthService } from './auth.service';
 import { JwtAuthGuard } from './guards/jwt-auth.guard';
 import { LocalAuthGuard } from './guards/local-auth.guard';
 
+@ApiTags('AuthController')
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @UseGuards(LocalAuthGuard)
+  @Post('login')
+  @ApiOkResponse({
+    description: 'Login efetuado com sucesso',
+    type: String,
+  })
   async login(@Request() req: any) {
     return this.authService.login(req.user);
   }
 
   @UseGuards(JwtAuthGuard)
-  @Post('test')
+  @Post('register')
+  @ApiOkResponse({
+    description: 'Registro efetuado com sucesso',
+    type: String,
+  })
   async register(@Request() req: any) {
     return req.user;
   }
